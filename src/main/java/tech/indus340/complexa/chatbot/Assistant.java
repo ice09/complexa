97c6bd4d033d5d75cfa3c3c680615b43e2b180f7
Alexander C. <alexander.culum@gmail.com>
2024-09-16 07:44:49 +0200

    Change dialog threshold.

--- src/main/java/tech/indus340/complexa/chatbot/Assistant.java
+++ src/main/java/tech/indus340/complexa/chatbot/Assistant.java
@@ -4,7 +4,12 @@ import dev.langchain4j.service.SystemMessage;
 
 public interface Assistant {
 
-    @SystemMessage("You are a helpful audio chat bot, keep your answers concise, except when asked to go into detail.")
+    @SystemMessage(
+        """
+        You are a helpful audio chat bot. 
+        Answer clearly and only add relevant information, use no fillers.
+        Keep answers concise, except when asked to go into detail.
+        """)
     String chat(String message);
 
 }
